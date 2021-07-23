4e461d0df3277bed2a1a7a6fcb528df565ae8458
vinayaksh42 <vinayaksh42@gmail.com>
2021-07-23 11:45:13 +0530

    added test for common component

---
+++ test/commonSvg.test.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-undef */
+import React from 'react'
+// import renderer from 'react-test-renderer'
+import { EOS_10K } from '../lib'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+afterEach(cleanup)
+
+it('Filled SVG Size prop test', () => {
+  const randomSize = Math.floor(Math.random()) + 1
+  const { getByTestId } = render(
+    <EOS_10K size={`${randomSize}`}/>
+  )
+  expect(getByTestId('eos-svg-component')).toHaveStyle({
+    width: `${randomSize}`,
+    height: `${randomSize}`
+  })
+})
+
+it('Filled SVG Color prop test', () => {
+  const { getByTestId } = render(
+    <EOS_10K color="blue"/>
+  )
+  expect(getByTestId('eos-svg-component')).toHaveAttribute('fill', 'blue')
+})
+
+it('Filled SVG Rotation prop test', () => {
+  const randomDegree = Math.floor(Math.random() * 360) + 1
+  const { getByTestId } = render(
+    <EOS_10K rotate={`${randomDegree}`}/>
+  )
+  expect(getByTestId('eos-svg-component')).toHaveAttribute('transform', `rotate(${randomDegree}) translate(0, 0) scale(1, 1)`)
+})
+
+it('Filled SVG horizontalFlip prop test', () => {
+  const { getByTestId } = render(
+    <EOS_10K horizontalFlip={true}/>
+  )
+  expect(getByTestId('eos-svg-component')).toHaveAttribute('transform', 'rotate(0) translate(24, 0) scale(-1, 1)')
+})
+
+it('Filled SVG verticalFlip prop test', () => {
+  const { getByTestId } = render(
+    <EOS_10K verticalFlip={true}/>
+  )
+  expect(getByTestId('eos-svg-component')).toHaveAttribute('transform', 'rotate(0) translate(0, 24) scale(1, -1)')
+})
