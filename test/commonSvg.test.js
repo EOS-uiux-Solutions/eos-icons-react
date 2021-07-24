2f367ad08c1266cab90a634e216d8548bcb450f2
vinayaksh42 <vinayaksh42@gmail.com>
2021-07-24 13:27:32 +0530

    new test cases for common outlined

--- test/commonSvg.test.js
+++ test/commonSvg.test.js
@@ -7,7 +7,8 @@ import '@testing-library/jest-dom/extend-expect'
 
 afterEach(cleanup)
 
-it('Filled SVG Size prop test', () => {
+// default - filled
+it('default filled component SVG Size prop test', () => {
   const randomSize = Math.floor(Math.random()) + 1
   const { getByTestId } = render(
     <EOS_10K size={`${randomSize}`}/>
@@ -18,14 +19,14 @@ it('Filled SVG Size prop test', () => {
   })
 })
 
-it('Filled SVG Color prop test', () => {
+it('default filled component SVG Color prop test', () => {
   const { getByTestId } = render(
     <EOS_10K color="blue"/>
   )
   expect(getByTestId('eos-svg-component')).toHaveAttribute('fill', 'blue')
 })
 
-it('Filled SVG Rotation prop test', () => {
+it('default filled component SVG Rotation prop test', () => {
   const randomDegree = Math.floor(Math.random() * 360) + 1
   const { getByTestId } = render(
     <EOS_10K rotate={`${randomDegree}`}/>
@@ -33,16 +34,57 @@ it('Filled SVG Rotation prop test', () => {
   expect(getByTestId('eos-svg-component')).toHaveAttribute('transform', `rotate(${randomDegree}) translate(0, 0) scale(1, 1)`)
 })
 
-it('Filled SVG horizontalFlip prop test', () => {
+it('default filled component SVG horizontalFlip prop test', () => {
   const { getByTestId } = render(
     <EOS_10K horizontalFlip={true}/>
   )
   expect(getByTestId('eos-svg-component')).toHaveAttribute('transform', 'rotate(0) translate(24, 0) scale(-1, 1)')
 })
 
-it('Filled SVG verticalFlip prop test', () => {
+it('default filled component SVG verticalFlip prop test', () => {
   const { getByTestId } = render(
     <EOS_10K verticalFlip={true}/>
+  )
+  expect(getByTestId('eos-svg-component')).toHaveAttribute('transform', 'rotate(0) translate(0, 24) scale(1, -1)')
+})
+
+// default - outlined
+it('default outlined component SVG Size prop test', () => {
+  const randomSize = Math.floor(Math.random()) + 1
+  const { getByTestId } = render(
+    <EOS_10K size={`${randomSize}`} theme="outlined"/>
+  )
+  expect(getByTestId('eos-svg-component')).toHaveStyle({
+    width: `${randomSize}`,
+    height: `${randomSize}`
+  })
+})
+
+it('default outlined component SVG Color prop test', () => {
+  const { getByTestId } = render(
+    <EOS_10K color="blue" theme="outlined"/>
+  )
+  expect(getByTestId('eos-svg-component')).toHaveAttribute('fill', 'blue')
+})
+
+it('default outlined component SVG Rotation prop test', () => {
+  const randomDegree = Math.floor(Math.random() * 360) + 1
+  const { getByTestId } = render(
+    <EOS_10K rotate={`${randomDegree}`} theme="outlined"/>
+  )
+  expect(getByTestId('eos-svg-component')).toHaveAttribute('transform', `rotate(${randomDegree}) translate(0, 0) scale(1, 1)`)
+})
+
+it('default outlined component SVG horizontalFlip prop test', () => {
+  const { getByTestId } = render(
+    <EOS_10K horizontalFlip={true} theme="outlined"/>
+  )
+  expect(getByTestId('eos-svg-component')).toHaveAttribute('transform', 'rotate(0) translate(24, 0) scale(-1, 1)')
+})
+
+it('default outlined component SVG verticalFlip prop test', () => {
+  const { getByTestId } = render(
+    <EOS_10K verticalFlip={true} theme="outlined"/>
   )
   expect(getByTestId('eos-svg-component')).toHaveAttribute('transform', 'rotate(0) translate(0, 24) scale(1, -1)')
 })
